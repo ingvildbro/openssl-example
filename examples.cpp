4389bd2db512af9a16d15505c3eb54f7b24ae806
ingvildbro <ingvild.broen@gmail.com>
2017-10-14 10:28:53 +0200

    Spacing in print

--- examples.cpp
+++ examples.cpp
@@ -7,30 +7,30 @@ int main() {
   cout << "SHA-1 with 1 iteration" << endl;
   cout << Crypto::hex(Crypto::sha1("Test")) << endl << endl;
   
-  cout << "SHA-1 with two iterations" << endl;
-  cout << Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) << endl;
+  cout << "SHA-1 with 2 iterations" << endl;
+  cout << Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) << endl << endl;
   
   // MD5
   cout << "MD5 with 1 iteration" << endl;
-  cout << Crypto::hex(Crypto::md5("Test")) << endl;
+  cout << Crypto::hex(Crypto::md5("Test")) << endl << endl;
   
-  cout << "MD5 with 2 iterations" << endl;
+  //cout << "MD5 with 2 iterations" << endl;
   //cout << Crypto::hex(Crypto::md5(Crypto::md5("Test"))) << endl;
   
   // SHA-256
   cout << "SHA-256 with 1 iteration" << endl;
-  cout << Crypto::hex(Crypto::sha256("Test")) << endl;
+  cout << Crypto::hex(Crypto::sha256("Test")) << endl << endl;
   
   cout << "SHA-256 with 2 iterations" << endl;
-  cout << Crypto::hex(Crypto::sha256(Crypto::sha256("Test"))) << endl;
+  cout << Crypto::hex(Crypto::sha256(Crypto::sha256("Test"))) << endl << endl;
   
   
   // SHA-512
   cout << "SHA-512 with 1 iteration" << endl;
-  cout << Crypto::hex(Crypto::sha512("Test")) << endl;
+  cout << Crypto::hex(Crypto::sha512("Test")) << endl << endl;
   
   cout << "SHA-512 with 2 iterations" << endl;
-  cout << Crypto::hex(Crypto::sha512(Crypto::sha512("Test"))) << endl;
+  cout << Crypto::hex(Crypto::sha512(Crypto::sha512("Test"))) << endl << endl;
   
   
 
