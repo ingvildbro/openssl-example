e0802100b3b612bfb00941b9b35d025ee220b396
ingvildbro <ingvild.broen@gmail.com>
2017-10-18 21:38:47 +0200

    implemented decrypt

--- examples.cpp
+++ examples.cpp
@@ -3,6 +3,39 @@
 
 using namespace std;
 
+
+string key = "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6";
+string salt = "Saltet til Ola";
+int iterations = 2048;
+int keylength = 160 / 8;
+string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+void Check(){
+  bool done = false;
+  while(done == false){
+    char password[3];
+    for (char p1 : alphabet) {
+      for (char p2 : alphabet) {
+        for (char p3 : alphabet) {
+          password[0] = p1;
+          password[1] = p2;
+          password[2] = p3;
+          cout << "curr: " << password << endl;
+          
+          if(key == Crypto::hex(Crypto::pbkdf2(password, salt, iterations, keylength))){
+            cout << "Password: " << password << endl;
+            done = true;
+            exit(0);
+          }
+        }
+      }
+    }
+  }
+  exit(0);
+}
+
+
+
 int main() {
   cout << "SHA-1 with 1 iteration" << endl;
   cout << Crypto::hex(Crypto::sha1("Test")) << endl << endl;
@@ -36,4 +69,11 @@ int main() {
 
   cout << "The derived key from the PBKDF2 algorithm" << endl;
   cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl;
+  
+  
+  
+  cout << "Pas key" << endl;
+  cout << Crypto::hex(Crypto::pbkdf2("QwE", salt, iterations , keylength)) << endl << endl ;
+  
+  Check();
 }
